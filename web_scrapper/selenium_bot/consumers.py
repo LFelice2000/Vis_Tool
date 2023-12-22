e58e86bd538df9404a152f29d8b9e06814726dee
Luis-Felice-Estudiante-UAM <luis.felice@estudiante.uam.es>
2023-12-22 12:31:16 +0100

    v1.3.1: socket concurrente (falta probar en pagina final)

--- web_scrapper/selenium_bot/consumers.py
+++ web_scrapper/selenium_bot/consumers.py
@@ -1,5 +1,4 @@
-from channels.consumer import SyncConsumer
-
+from channels.generic.websocket import AsyncWebsocketConsumer
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -16,8 +15,189 @@ import json
 import time
 import os
 import pandas as pd
+import asyncio
+import functools
 
-class EchoConsumer(SyncConsumer):
+def web_login_local(receive_payload, wd, wd_wait):
+   
+  wd.get("http://localhost:8080/login/index.php")
+
+  time.sleep(4)
+  wd_wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'login-identityprovider-btn')))
+
+  wd.find_element(By.CLASS_NAME, 'login-identityprovider-btn').click()
+
+  time.sleep(4)
+  wd_wait.until(EC.presence_of_element_located((By.ID, 'i0116')))
+  
+  wd.find_element(By.ID, 'i0116').send_keys(receive_payload['username'])
+  wd.find_element(By.ID, 'idSIButton9').click()
+
+  time.sleep(4)
+  wd_wait.until(EC.presence_of_element_located((By.ID, 'i0118')))
+
+  wd.find_element(By.ID, 'i0118').send_keys(receive_payload['password'])
+  wd.find_element(By.ID, 'idSIButton9').click()
+
+  wd_wait.until(EC.presence_of_element_located((By.ID, 'idRichContext_DisplaySign')))
+
+  token = wd.find_element('id', 'idRichContext_DisplaySign').text
+  
+  return token
+
+def web_scrap_local(receive_payload, wd, wd_wait):
+
+  courseId = receive_payload['courseId'].replace("/", "")
+
+  wd.get(f"http://localhost:8080/grade/export/xls/index.php?id={courseId}")
+
+  time.sleep(4)
+
+  wd.find_element('id', 'id_submitbutton').click()
+
+  time.sleep(4)
+
+  wd.get(f"http://localhost:8080/course/view.php?id={courseId}")
+
+  urls = wd.find_elements(By.TAG_NAME, "a")
+
+  attendaceFilter = lambda elem: 'http://localhost:8080/mod/attendance' in elem.get_attribute('href') if elem.get_attribute('href') is not None else None
+  filteredElems = filter(attendaceFilter, urls)
+  attendanceUrl = next(filteredElems, None)
+
+  attendanceId = attendanceUrl.get_attribute("href").split("?")[1].split("=")[1]
+
+  wd.get(f"http://localhost:8080/mod/attendance/export.php?id={attendanceId}")
+
+  wd_wait.until(EC.element_to_be_clickable((By.ID, "id_includenottaken")))
+
+  wd.find_element(By.ID, "id_includenottaken").click()
+  wd.find_element(By.ID, "id_submitbutton").click()
+
+  time.sleep(3)
+
+  wd.delete_all_cookies()
+
+  wd.close()
+
+  courseFiles = os.listdir(os.path.join(BASE_DIR, "courseFiles"))
+  
+  courseName = receive_payload['courseName']
+
+  courseData = pd.read_excel(os.path.join(BASE_DIR, f"courseFiles/{courseName} Calificaciones.xlsx"))
+  activitiesDataframe = pd.DataFrame(courseData)
+
+  courseAttendance = pd.read_excel(os.path.join(BASE_DIR, f"courseFiles/{courseFiles[0] if courseFiles[0] != f'{courseName} Calificaciones.xlsx' else courseFiles[1]}"), skiprows=[0,1,2])
+  attendanceDataframe = pd.DataFrame(courseAttendance)
+
+  for file in os.listdir(os.path.join(BASE_DIR, "courseFiles")):
+      os.remove(os.path.join(BASE_DIR, f"courseFiles/{file}"))
+
+  return {"activitiesDataframe": activitiesDataframe, "attendanceDataframe": attendanceDataframe}
+
+def web_login(receive_payload, wd, wd_wait):
+   
+  wd.get("https://moodle.uam.es/login/index.php")
+
+  time.sleep(4)
+  wd_wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'login-identityprovider-btn')))
+
+  wd.find_element(By.CLASS_NAME, 'login-identityprovider-btn').click()
+
+  time.sleep(4)
+  wd_wait.until(EC.presence_of_element_located((By.ID, 'i0116')))
+  
+  wd.find_element(By.ID, 'i0116').send_keys(receive_payload['username'])
+  wd.find_element(By.ID, 'idSIButton9').click()
+
+  time.sleep(4)
+  wd_wait.until(EC.presence_of_element_located((By.ID, 'i0118')))
+
+  wd.find_element(By.ID, 'i0118').send_keys(receive_payload['password'])
+  wd.find_element(By.ID, 'idSIButton9').click()
+
+  wd_wait.until(EC.presence_of_element_located((By.ID, 'idRichContext_DisplaySign')))
+
+  token = wd.find_element('id', 'idRichContext_DisplaySign').text
+  
+  return token
+
+def web_token_confirm(wd, wd_wait):
+    
+    try:
+      WebDriverWait(wd, 64).until(EC.presence_of_all_elements_located((By.ID, 'idDiv_SAASTO_Trouble')))
+    except TimeoutException:
+
+      return None
+    
+    wd.get("https://moodle.uam.es/login/index.php")
+
+    time.sleep(4)
+    wd_wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'login-identityprovider-btn')))
+
+    wd.find_element(By.CLASS_NAME, 'login-identityprovider-btn').click()
+
+    wd_wait.until(EC.presence_of_element_located((By.ID, 'idRichContext_DisplaySign')))
+
+    token = wd.find_element('id', 'idRichContext_DisplaySign').text
+
+    return token
+
+def web_scrap(receive_payload, wd, wd_wait):
+
+  courseId = receive_payload['courseId'].replace("/", "")
+
+  wd.get("https://moodle.uam.es/auth/saml2/login.php?wants&idp=cebe5294a678ea658b3001066ac8533e&passive=off")
+
+  time.sleep(15)
+
+  wd.get(f"https://moodle.uam.es/grade/export/xls/index.php?id={courseId}")
+
+  time.sleep(4)
+
+  wd.find_element('id', 'id_submitbutton').click()
+
+  time.sleep(4)
+
+  wd.get(f"https://moodle.uam.es/course/view.php?id={courseId}")
+
+  urls = wd.find_elements(By.TAG_NAME, "a")
+
+  attendaceFilter = lambda elem: 'https://moodle.uam.es/mod/attendance' in elem.get_attribute('href') if elem.get_attribute('href') is not None else None
+  filteredElems = filter(attendaceFilter, urls)
+  attendanceUrl = next(filteredElems, None)
+
+  attendanceId = attendanceUrl.get_attribute("href").split("?")[1].split("=")[1]
+
+  wd.get(f"https://moodle.uam.es/mod/attendance/export.php?id={attendanceId}")
+
+  wd_wait.until(EC.element_to_be_clickable((By.ID, "id_includenottaken")))
+
+  wd.find_element(By.ID, "id_includenottaken").click()
+  wd.find_element(By.ID, "id_submitbutton").click()
+
+  time.sleep(3)
+
+  wd.delete_all_cookies()
+
+  wd.close()
+
+  courseFiles = os.listdir(os.path.join(BASE_DIR, "courseFiles"))
+  
+  courseName = receive_payload['courseName']
+
+  courseData = pd.read_excel(os.path.join(BASE_DIR, f"courseFiles/{courseName} Calificaciones.xlsx"))
+  activitiesDataframe = pd.DataFrame(courseData)
+
+  courseAttendance = pd.read_excel(os.path.join(BASE_DIR, f"courseFiles/{courseFiles[0] if courseFiles[0] != f'{courseName} Calificaciones.xlsx' else courseFiles[1]}"), skiprows=[0,1,2])
+  attendanceDataframe = pd.DataFrame(courseAttendance)
+
+  for file in os.listdir(os.path.join(BASE_DIR, "courseFiles")):
+      os.remove(os.path.join(BASE_DIR, f"courseFiles/{file}"))
+
+  return {"activitiesDataframe": activitiesDataframe, "attendanceDataframe": attendanceDataframe}
+
+class EchoConsumer(AsyncWebsocketConsumer):
     
     WEB_API_PATH = f"{Path(__file__).resolve().parent.parent.parent.absolute()}/web_api/"
     print(os.path.join(WEB_API_PATH, "courseFiles"))
@@ -30,138 +210,41 @@ class EchoConsumer(SyncConsumer):
     wd_options.add_argument("--disable-dev-shm-usage")
     wd_options.add_argument('--headless')
     
-    
-    def websocket_connect(self, event):
+    async def websocket_connect(self, event):
       print("[socket] connect event called")
 
       self.wd = webdriver.Chrome(options=self.wd_options, service=self.wd_service)
       self.wd_wait = WebDriverWait(self.wd, 20)
 
-      self.send({
-         'type': 'websocket.accept'
-      })
+      await self.accept()
 
-    def websocket_receive(self, event):
+    async def websocket_receive(self, event):
       print("[socket] Empezando...")
+
+      loop = asyncio.get_event_loop()
 
       receive_payload = json.loads(event.get('text'))
 
-      print(f"payload: {receive_payload}")
-
       if(receive_payload['type'] == 'login'):
-         
-        self.wd.get("https://moodle.uam.es/login/index.php")
-
-        time.sleep(4)
-        self.wd_wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'login-identityprovider-btn')))
-
-        self.wd.find_element(By.CLASS_NAME, 'login-identityprovider-btn').click()
-
-        time.sleep(4)
-        self.wd_wait.until(EC.presence_of_element_located((By.ID, 'i0116')))
         
-        self.wd.find_element(By.ID, 'i0116').send_keys(receive_payload['username'])
-        self.wd.find_element(By.ID, 'idSIButton9').click()
+        token = await loop.run_in_executor(None, functools.partial(web_login, receive_payload, self.wd, self.wd_wait))
 
-        time.sleep(4)
-        self.wd_wait.until(EC.presence_of_element_located((By.ID, 'i0118')))
+        while token != None:
 
-        self.wd.find_element(By.ID, 'i0118').send_keys(receive_payload['password'])
-        self.wd.find_element(By.ID, 'idSIButton9').click()
+          send_payload = {"type": "token", "token": token}
+          await self.send(json.dumps(send_payload))
 
-        self.wd_wait.until(EC.presence_of_element_located((By.ID, 'idRichContext_DisplaySign')))
-
-        token = self.wd.find_element('id', 'idRichContext_DisplaySign').text
-        
-        while True:
-            
-            print(f"Your token is {token}")
-
-            send_payload = {"type": "token", "token": token}
-            self.send({
-            'type': 'websocket.send',
-            'text': json.dumps(send_payload)
-            })
-
-            try:
-                WebDriverWait(self.wd, 64).until(EC.presence_of_all_elements_located((By.ID, 'idDiv_SAASTO_Trouble')))
-            except TimeoutException:
-                break
-            
-            self.wd.get("https://moodle.uam.es/login/index.php")
-
-            time.sleep(4)
-            self.wd_wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'login-identityprovider-btn')))
-
-            self.wd.find_element(By.CLASS_NAME, 'login-identityprovider-btn').click()
-
-            self.wd_wait.until(EC.presence_of_element_located((By.ID, 'idRichContext_DisplaySign')))
-
-            token = self.wd.find_element('id', 'idRichContext_DisplaySign').text
+          token = await loop.run_in_executor(None, functools.partial(web_token_confirm, self.wd, self.wd_wait))
 
         send_payload = {"type": "token_success"}
-        self.send({
-        'type': 'websocket.send',
-        'text': json.dumps(send_payload)
-        })
+        await self.send(json.dumps(send_payload))
         
-        courseId = receive_payload['courseId'].replace("/", "")
+        courseInfo =  await loop.run_in_executor(None, functools.partial(web_scrap, receive_payload, self.wd, self.wd_wait))
 
-        self.wd.get("https://moodle.uam.es/auth/saml2/login.php?wants&idp=cebe5294a678ea658b3001066ac8533e&passive=off")
-
-        time.sleep(15)
-
-        self.wd.get(f"https://moodle.uam.es/grade/export/xls/index.php?id={courseId}")
-
-        time.sleep(4)
-
-        self.wd.find_element('id', 'id_submitbutton').click()
-
-        time.sleep(4)
-
-        self.wd.get(f"https://moodle.uam.es/course/view.php?id={courseId}")
-
-        urls = self.wd.find_elements(By.TAG_NAME, "a")
-
-        attendaceFilter = lambda elem: 'https://moodle.uam.es/mod/attendance' in elem.get_attribute('href') if elem.get_attribute('href') is not None else None
-        filteredElems = filter(attendaceFilter, urls)
-        attendanceUrl = next(filteredElems, None)
-
-        attendanceId = attendanceUrl.get_attribute("href").split("?")[1].split("=")[1]
-
-        self.wd.get(f"https://moodle.uam.es/mod/attendance/export.php?id={attendanceId}")
-
-        self.wd_wait.until(EC.element_to_be_clickable((By.ID, "id_includenottaken")))
-
-        self.wd.find_element(By.ID, "id_includenottaken").click()
-        self.wd.find_element(By.ID, "id_submitbutton").click()
-
-        time.sleep(3)
-
-        self.wd.delete_all_cookies()
-
-        self.wd.close()
-
-        courseFiles = os.listdir(os.path.join(BASE_DIR, "courseFiles"))
-        
-        courseName = receive_payload['courseName']
-
-        courseData = pd.read_excel(os.path.join(BASE_DIR, f"courseFiles/{courseName} Calificaciones.xlsx"))
-        activitiesDataframe = pd.DataFrame(courseData)
-
-        courseAttendance = pd.read_excel(os.path.join(BASE_DIR, f"courseFiles/{courseFiles[0] if courseFiles[0] != f'{courseName} Calificaciones.xlsx' else courseFiles[1]}"), skiprows=[0,1,2])
-        attendanceDataframe = pd.DataFrame(courseAttendance)
-
-        for file in os.listdir(os.path.join(BASE_DIR, "courseFiles")):
-            os.remove(os.path.join(BASE_DIR, f"courseFiles/{file}"))
-
-        send_payload = {"type": "scrap_success", "activities": activitiesDataframe.to_json(orient='records', force_ascii=False, default_handler=str), "attendance": attendanceDataframe.to_json(orient='records', force_ascii=False, default_handler=str)}
-        self.send({
-        'type': 'websocket.send',
-        'text': json.dumps(send_payload)
-        })
+        send_payload = {"type": "scrap_success", "activities": courseInfo["activitiesDataframe"].to_json(orient='records', force_ascii=False, default_handler=str), "attendance": courseInfo["attendanceDataframe"].to_json(orient='records', force_ascii=False, default_handler=str)}
+        await self.send(json.dumps(send_payload))
     
-    def websocket_disconnect(self, event):
+    async def websocket_disconnect(self, event):
       print("[socket] connection closed")
 
     
