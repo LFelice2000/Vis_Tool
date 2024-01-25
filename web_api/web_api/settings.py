06c92cdc29e474a522752fa4dd2e1675646c0652
Luis-Felice-Estudiante-UAM <luis.felice@estudiante.uam.es>
2024-01-25 14:08:19 +0100

    est final app

--- web_api/web_api/settings.py
+++ web_api/web_api/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'alefelice01',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
