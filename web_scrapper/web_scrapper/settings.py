1fa80e5f80c45a01359a3b6700e0d5fc1b1e0d83
Luis-Felice-Estudiante-UAM <luis.felice@estudiante.uam.es>
2024-04-16 12:58:21 +0200

    Avoiding database error

--- web_scrapper/web_scrapper/settings.py
+++ web_scrapper/web_scrapper/settings.py
@@ -83,12 +83,7 @@ ASGI_APPLICATION = 'web_scrapper.routing.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {}
 
 
 # Password validation
